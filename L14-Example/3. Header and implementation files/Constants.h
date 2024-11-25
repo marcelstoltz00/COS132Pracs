bbcd0fa25a4d7cbe348b22f47142f2fee3a8b41a
marcelstoltz00 <139484173+marcelstoltz00@users.noreply.github.com>
2024-11-25 21:48:49 +0200

    Delete L14-Example directory

--- L14-Example/3. Header and implementation files/Constants.h
+++
@@ -1,6 +0,0 @@
-#ifndef Constants_h
-#define Constants_h
-
-	const float PI = 3.141592653589793238;
-
-#endif /* Constants_h */
--- L14-Example/3. Header and implementation files/LECTURE16/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef main_h
-#define main_h
-double calculateCircleArea(double);
-
-
-#endif
